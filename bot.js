a3cd832122b6f4d881aa34b692155e1551a2288a
Programy1 <33522372+programy1@users.noreply.github.com>
2020-05-28 18:25:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-pg.connect(connectionString, function(err, client, done) {
-   client.query('SELECT * FROM your_table', function(err, result) {
-      done();
-      if(err) return console.error(err);
-      console.log(result.rows);
-   });
-});
+
 
 client.on('ready', () => {
     console.log('Bot ready!');
