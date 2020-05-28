f7167bd936ef68b3c5da9870d0e1ee450c6dbc6a
Programy1 <33522372+programy1@users.noreply.github.com>
2020-05-28 21:03:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -79,8 +79,8 @@ let data = '';
     console.log(JSON.parse(data).explanation);
        pole = [];
        for(let i = 0; i < JSON.parse(data).length; i++) {
-           // pole.push({name: JSON.parse(data)[i].kdo, value: JSON.parse(data)[i].kolik});
-      pole.push({name: "Jméno1", value: "Počet hlasů: 0"});
+            pole.push({name: i+1 + ') ' + JSON.parse(data)[i].kdo, value: JSON.parse(data)[i].kolik});
+      //pole.push({name: "Jméno1", value: "Počet hlasů: 0"});
        }
            message.reply({embed: {
     color: 3447003,
