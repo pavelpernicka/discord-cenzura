1a1b6bcc092f815f74f86138dae82f31b5c17dc5
Programy1 <33522372+programy1@users.noreply.github.com>
2020-05-28 19:13:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const https = require('https');
 
 client.on('ready', () => {
     console.log('Bot ready!');
@@ -16,6 +17,24 @@ client.on('message', message => {
                  let user = message.mentions.users.first();
                  message.reply('Opravdu chceš zvolit <@' + user.id + '>?');
                  
+                https.get('http://www.programy1.borec.cz/gjs-meteo/discord-volby.php?kdo=1&koho=' + user.id, (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data).explanation);
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
+                
+                
                 
             }else{
                 message.reply('Uživatel, kterého chceš zvolit, neexistuje!');
