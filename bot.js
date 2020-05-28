8352d5e346b73fe06a9542ea1b99cdda70c2fcf7
Programy1 <33522372+programy1@users.noreply.github.com>
2020-05-28 19:45:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -63,6 +63,8 @@ client.on('message', message => {
                 message.reply('Uživatel, kterého chceš zvolit, neexistuje!');
             }
         }
+    }elseif(firstWord === 'pořadí'){
+        message.reply('Aktuální pořadí:');
     }
 
 });
